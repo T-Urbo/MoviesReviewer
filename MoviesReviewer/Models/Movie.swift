e6ebc1dd5b71adb22a783565d7c9dd8eb24d047f
T-Urbo <urbanovich.tima@gmail.com>
2022-04-22 01:51:59 +0200

    Added: Maded working function for parsing JSON data using API from imdb-api.com

--- MoviesReviewer/Models/Movie.swift
+++ MoviesReviewer/Models/Movie.swift
@@ -5,17 +5,34 @@
 //  Created by Timothey Urbanovich on 18/04/2022.
 //
 
+import UIKit
 import Foundation
 
-var movieName: String = ""
+var movieName: String = "inception 2010"
 var APIKey: String = "k_rae8rk7s"
 var searchLink: String = "https://imdb-api.com/en/API/SearchMovie/"
 var APIRequest = searchLink + APIKey + "/" + movieName
+var apiURL = URL(string: APIRequest)
 
 struct MovieResponce: Decodable {
     
 }
 
-struct Movie: Decodable {
-    
+struct Response: Decodable {
+    let searchType: String
+    let expression: String
+    let results: [Results]
+    let errorMessage: String
 }
+
+struct Results: Decodable {
+    let id: String
+    let resultType: String
+    let image: URL
+    let title: String
+    let description: String
+}
+
+/*
+ {"searchType":"Movie","expression":"inception 2010","results":[{"id":"tt1375666","resultType":"Title","image":"https://imdb-api.com/images/original/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6800_AL_.jpg","title":"Inception","description":"(2010)"},{"id":"tt1790736","resultType":"Title","image":"https://imdb-api.com/images/original/MV5BMjE0NGIwM2EtZjQxZi00ZTE5LWExN2MtNDBlMjY1ZmZkYjU3XkEyXkFqcGdeQXVyNjMwNzk3Mjk@._V1_Ratio0.6800_AL_.jpg","title":"Inception: Motion Comics","description":"(2010 Video)"},{"id":"tt5295990","resultType":"Title","image":"https://imdb-api.com/images/original/MV5BZGFjOTRiYjgtYjEzMS00ZjQ2LTkzY2YtOGQ0NDI2NTVjOGFmXkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_Ratio0.6800_AL_.jpg","title":"Inception: Jump Right Into the Action","description":"(2010 Video)"},{"id":"tt12960252","resultType":"Title","image":"https://imdb-api.com/images/original/nopicture.jpg","title":"Inception Premiere","description":"(2010)"},{"id":"tt1686778","resultType":"Title","image":"https://imdb-api.com/images/original/nopicture.jpg","title":"Inception: 4Movie Premiere Special","description":"(2010 TV Movie)"}],"errorMessage":""}
+ */
--- MoviesReviewer/ViewController.swift
+++ MoviesReviewer/ViewController.swift
@@ -21,13 +21,34 @@ class ViewController: UIViewController, UISearchResultsUpdating {
     @IBOutlet weak var backgroundImageView: UIImageView!
     
     let searchController = UISearchController(searchResultsController: ResultsViewController())
+    // let searchHistory = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Setup the ViewController
+        
         title = "Search"
         searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
         navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
         print(APIRequest)
+        let url = "https://imdb-api.com/en/API/SearchMovie/k_rae8rk7s/inception%202010"
+        getData(from: url)
+//        if let url = URL(string: "https://imdb-api.com/en/API/SearchMovie/k_rae8rk7s/inception%202010") {
+//            URLSession.shared.dataTask(with: url) {
+//                data, responce, error in
+//                if let data = data {
+//                    if let jsonString = String(data: data, encoding: .utf8) {
+//                            print(jsonString)
+//                    }
+//                }
+//            }.resume()
+//        }
+        
+//        parseJSON(url: url)
         
         
     }
@@ -40,5 +61,41 @@ class ViewController: UIViewController, UISearchResultsUpdating {
         print(text)
     }
     
-    
+    private func getData(from url: String) {
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("Something went wrong")
+                return
+            }
+            
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print(String(describing: error))
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.results)
+            print(json.expression)
+            print(json.searchType)
+            json.results.forEach { results in print(results.id)
+                print(results.resultType)
+                print(results.image)
+                print(results.title)
+            }
+             
+//            print(json.results.description)
+        })
+        
+        task.resume()
+        
+    }
 }
+
+
